8aacc11ae03aea6db0d3cb8655cdc9df67c1a32e
jjuaien <1693104343@qq.com>
2024-08-25 00:28:46 +0800

    实现列表筛选功能

--- src/views/SubCategory/index.vue
+++ src/views/SubCategory/index.vue
@@ -26,6 +26,12 @@ const getGoodList = async() => {
   goodList.value = res.result.items
 }
 onMounted(() => getGoodList())
+
+//tab切换回调函数
+const tabChange = () => {
+  reqData.value.page = 1
+  getGoodList()
+}
 </script>
 
 <template>
@@ -41,7 +47,7 @@ onMounted(() => getGoodList())
     </div>
     <div class="sub-container">
       <!-- 筛选框 -->
-      <el-tabs>
+      <el-tabs v-model="reqData.sortField" @tab-change="tabChange">
         <el-tab-pane label="最新商品" name="publishTime"></el-tab-pane>
         <el-tab-pane label="最高人气" name="orderNum"></el-tab-pane>
         <el-tab-pane label="评论最多" name="evaluateNum"></el-tab-pane>
